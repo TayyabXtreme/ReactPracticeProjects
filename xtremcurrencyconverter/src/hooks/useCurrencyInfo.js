42c7463e34234cc87d074dffd0f22eae1c29afeb
Tayyab Xtreme <tayyabxtrem@gmail.com>
2024-06-12 22:52:39 +0500

    currency convertor

---
+++ xtremcurrencyconverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,26 @@
+import { useEffect , useState } from 'react';
+
+
+
+
+
+
+function useCurrencyInfo(currency){
+    const [data, setData] = useState({})
+    useEffect(() => {
+        fetch(`https://2024-03-06.currency-api.pages.dev/v1/currencies/${currency}.json`)
+        .then(response => response.json())
+        .then(res => {
+            setData(res[currency])
+            console.log(data)
+        })
+        .catch(error => console.log(error))
+    }
+    , [currency])
+    console.log(data)    
+
+    return data
+
+}
+
+export default useCurrencyInfo
