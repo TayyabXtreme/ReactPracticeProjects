b0171560689c5b5797724ff290810851b51c9a0f
Tayyab Xtreme <tayyabxtrem@gmail.com>
2024-06-13 17:41:00 +0500

    HEader and footer added

---
+++ routerreactproject/src/Layout.jsx
@@ -0,0 +1,19 @@
+
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import { Outlet } from 'react-router-dom'
+
+
+
+const Layout = () => {
+  return (
+    <>
+        <Header/>
+        <Outlet/>
+        <Footer/>
+
+    </>
+  )
+}
+
+export default Layout
