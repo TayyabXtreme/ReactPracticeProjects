76c4f6653e3b5b3cbaeb292ad5baa8b2589ca337
Tayyab Xtreme <tayyabxtrem@gmail.com>
2024-06-13 22:12:37 +0500

    new mini project added

---
+++ themeswitcheroncontextapi/src/App.jsx
@@ -0,0 +1,24 @@
+
+function App() {
+ 
+  return (
+    <>
+     <h1 className="text-center bg-pink-400 text-white text-4xl my-10 w-1/2 m-auto p-4">Hello Xtrem</h1>
+     
+<div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        {/* Theme button components */}
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       {/* Card */}
+                    </div>
+                </div>
+            </div>
+
+    </>
+  )
+}
+
+export default App
