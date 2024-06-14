e0dee68f54f0070d133e134c8737b557b01a99a7
Tayyab Xtreme <tayyabxtrem@gmail.com>
2024-06-14 12:17:14 +0500

    pic change

--- themeswitcheroncontextapi/src/components/Card.jsx
+++ themeswitcheroncontextapi/src/components/Card.jsx
@@ -7,7 +7,7 @@ export default function Card() {
     return (
         <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="/">
-                <img className="p-8 rounded-t-lg" src={myImage} />
+                <img className="p-8 rounded-t-lg m-auto" src={myImage}  width={200}/>
             </a>
             <div className="px-5 pb-5">
                 <a href="/">
