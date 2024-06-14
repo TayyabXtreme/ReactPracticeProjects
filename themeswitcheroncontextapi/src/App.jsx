5318eab4524e7429b547ee31005e84eea43a18d4
Tayyab Xtreme <tayyabxtrem@gmail.com>
2024-06-14 11:48:56 +0500

    completed

--- themeswitcheroncontextapi/src/App.jsx
+++ themeswitcheroncontextapi/src/App.jsx
@@ -1,22 +1,41 @@
+import { useEffect, useState } from "react"
+import { ThemeProvider } from "./context/theme"
+import Card from "./components/Card"
+import ThemeBtn from "./components/ThemeBtn"
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme = () => {
+    setThemeMode("light")
+  }
+
+  const darkTheme = () => {
+    setThemeMode("dark")
+  }
+
+  useEffect(() => {
+    document.querySelector('html').classList.remove('light','dark')
+    document.querySelector('html').classList.add(themeMode)
+  }
+  , [themeMode])
  
   return (
     <>
-     <h1 className="text-center bg-pink-400 text-white text-4xl my-10 w-1/2 m-auto p-4">Hello Xtrem</h1>
+    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
      
 <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        {/* Theme button components */}
+                       <ThemeBtn/>
                     </div>
 
                     <div className="w-full max-w-sm mx-auto">
-                       {/* Card */}
+                      <Card />
                     </div>
                 </div>
-            </div>
-
+</div>
+    </ThemeProvider>
     </>
   )
 }
