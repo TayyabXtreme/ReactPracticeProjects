83e630661298061cbd29747d3de1d6b0cfafe569
Tayyab Xtreme <tayyabxtrem@gmail.com>
2024-06-15 22:32:28 +0500

    redux todo app

---
+++ reduxtoolkittodo/src/App.jsx
@@ -0,0 +1,20 @@
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todo from './components/Todo'
+
+
+function App() {
+  
+
+  return (
+    <>
+
+     <h1>Leanr about redux toolkit</h1>
+     <AddTodo/>
+     <Todo/>
+    </>
+  )
+}
+
+export default App
