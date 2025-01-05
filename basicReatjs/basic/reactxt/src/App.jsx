5d04a7ac94c8f31d7f7c62294efdc9fedf20eb55
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-01-05 18:29:19 +0500

    react router

--- basicReatjs/basic/reactxt/src/App.jsx
+++ basicReatjs/basic/reactxt/src/App.jsx
@@ -1,36 +1,25 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/home'
+import About from './pages/about'
+import Contact from './pages/contact'
+import Product from './pages/product'
+import NavBar from './components/NavBar'
 
-const App =() => {
-
-  const [data,getData]=React.useState([])
-
-  const getall=async ()=>{
-    console.log('Get Data')
-    const response=await axios.get('https://picsum.photos/v2/list?page=2&limit=10')
-    getData(response.data)
-    console.log(response.data)
-  }
-
-  useEffect(()=>{
-    getall()
-  },[])
-
+const App = () => {
   return (
-    <div className='p-10'>
-     
-      <div className='p-5 b-gray-900 flex flex-wrap'>
-        {
-          data.map((item,index)=>{
-           return <div className='border-2 border-gray-900 p-5 m-5 flex item-center justify-center flex-col' key={index}>
-            <h1>{item.author}</h1>
-            <img className='w-24 h-24' src={item.download_url} alt="" />
+    <div>
 
+    <NavBar></NavBar>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      
+      <Route path="/home" element={<Home />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="" element={<Product />} />
 
-            </div>
-          })
-        }
-      </div>
+    </Routes>
     </div>
   )
 }
--- basicReatjs/basic/reactxt/src/components/NavBar.jsx
+++ basicReatjs/basic/reactxt/src/components/NavBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
     <div><nav className=' bg-emerald-600 w-full flex py-5 px-10 items-center justify-between'>
-    <h2>ATX</h2>
+    <Link to='/'><h2>ATX</h2></Link>
     <div className='flex items-center justify-around w-1/2'>
-      <h4>About</h4>
-      <h4>Contact</h4>
+    <Link to='/about'>  <h4>About</h4> </Link>
+    <Link to='/contact'>  <h4>Contact</h4></Link>
       <h4>Services</h4>
       <h4>Your Account</h4>
     </div>
