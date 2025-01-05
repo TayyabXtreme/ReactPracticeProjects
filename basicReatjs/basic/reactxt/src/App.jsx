17635a8c12de9da572a7e3cf9e4dff3496f51217
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-01-05 18:13:32 +0500

    integrating api and useeffect

--- basicReatjs/basic/reactxt/src/App.jsx
+++ basicReatjs/basic/reactxt/src/App.jsx
@@ -1,25 +1,37 @@
-import React from 'react'
-import { useState } from 'react';
-import Hello from './components/Hello';
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
-import Card from './components/Card';
+import axios from 'axios'
+import React, { useEffect } from 'react'
 
-const App = () => {
-  
-  const [userName, setuserName] = useState("")
+const App =() => {
+
+  const [data,getData]=React.useState([])
+
+  const getall=async ()=>{
+    console.log('Get Data')
+    const response=await axios.get('https://picsum.photos/v2/list?page=2&limit=10')
+    getData(response.data)
+    console.log(response.data)
+  }
+
+  useEffect(()=>{
+    getall()
+  },[])
+
   return (
-  
-   <>
-   
-   <NavBar/>
-   {/* <h1 className='h-svh'></h1> */}
-   <div className='flex'> 
-   <Card name="Tayyab" description={"This is description"}/>  
-   <Card name="Anees" description={"This is description"}/>
-   </div>
-   <Footer/>
-   </>
+    <div className='p-10'>
+     
+      <div className='p-5 b-gray-900 flex flex-wrap'>
+        {
+          data.map((item,index)=>{
+           return <div className='border-2 border-gray-900 p-5 m-5 flex item-center justify-center flex-col' key={index}>
+            <h1>{item.author}</h1>
+            <img className='w-24 h-24' src={item.download_url} alt="" />
+
+
+            </div>
+          })
+        }
+      </div>
+    </div>
   )
 }
 
