4f53b28f846a9c2e2263742dd732a1623171bf5b
Muhammad Tayyab <tayyabxtrem@gmail.com>
2025-01-05 16:59:27 +0500

    two way binding

--- basicReatjs/basic/reactxt/src/App.jsx
+++ basicReatjs/basic/reactxt/src/App.jsx
@@ -1,22 +1,23 @@
-import React, { useState } from 'react'
+import React from 'react'
+import { useState } from 'react';
 
 const App = () => {
-  
-  const [user, setuser] = useState("Tayyab")
-  const changeUser=()=>{
-    setuser("Anees")
-    
-    
+  const sumbitHandler = (e) => {
+    e.preventDefault();
+    console.log('submit')
   }
+  const [userName, setuserName] = useState("")
   return (
-    
    <div>
-    <h1 className='text-5xl'>UserName is {user}</h1>
-    <button onClick={changeUser}>Change user</button>
-
-    <hr />
-    <hr />
-      <h1>Hello Guys</h1>
+    <form onSubmit={(e)=>sumbitHandler(e)}>
+      <input 
+      className='px-4  rounded  text-black py-3 text-xl m-5' 
+      type="text"
+      value={userName}
+      onChange={(e)=>setuserName( e.target.value)}
+      />
+      <input className='px-4 py-3 text-xl font-semibold bg-emerald-600 border-1 rounded' type="submit" />
+    </form>
    </div>
   )
 }
